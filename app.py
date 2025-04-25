93f22928376d008e90e51016100bbed71c0763ed
sananwar <58251699+sananwar@users.noreply.github.com>
2025-04-25 12:01:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 import streamlit as st
-from data import get_data
 
-st.title("🎈 Simpele Streamlit App met Data")
+# Titel van de app
+st.title("Mijn eerste Streamlit app")
 
-# Data ophalen via data.py
-df = get_data()
+# Invoer van de gebruiker
+naam = st.text_input("Wat is je naam?")
 
-st.write("Hier is de data uit het andere bestand:")
-st.dataframe(df)
-
-# Extra interactie
-stad = st.selectbox("Filter op stad:", df["Stad"].unique())
-
-filtered_df = df[df["Stad"] == stad]
-st.write("Geselecteerde resultaten:")
-st.table(filtered_df)
-
-# streamlit run app.py
+# Als er een naam is ingevuld, geef een begroeting
+if naam == "Ruveyda":
+    st.success(f"Salam Alaikom habiba {naam}! Ik hou van jou! <3 xoxoxox")
+else:
+    st.info("Fy bitch")
